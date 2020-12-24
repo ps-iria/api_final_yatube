1fbe4eac9d5b79769e9239180cf99e79542baaad
drpsychps <68935465+drpsychps@users.noreply.github.com>
2020-12-24 12:50:18 +0700

    Done

--- api/serializers.py
+++ api/serializers.py
@@ -8,7 +8,7 @@ class PostSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source='author.username')
 
     class Meta:
-        fields = ('id', 'text', 'author', 'pub_date')
+        fields = '__all__'
         model = Post
 
 
